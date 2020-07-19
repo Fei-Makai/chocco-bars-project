f09714b30b24561a28285a08ee3e43427fbc264c
Mikhail Fefelov <fefelov.michael@gmail.com>
2020-07-19 16:36:41 +0300

    Доделан видеоплейер, добавлены переходы с описания батончиков на секцию Заказ, исправлены баги с меню

--- scripts/ops.js
+++ scripts/ops.js
@@ -1,89 +1,122 @@
 const sections = $('section');
 const display = $('.maincontent');
+const sideMenu = $('.fixed-menu');
+const menuItems = sideMenu.find('.fixed-menu__item');
+
+const mobileDetect = new MobileDetect(window.navigator.userAgent);
+const isMobile = mobileDetect.mobile();
 
 let inScroll = false;
 
 sections.first().addClass('active');
 
-const performTransition = sectionEq => {
-    if (inScroll == false) {
-        inScroll = true;
-        const position = sectionEq * -100;
+const countSectionPosition = sectionEq => {
+    const position = sectionEq * -100;
 
-        const currentSection = sections.eq(sectionEq);
-        const menuTheme = currentSection.attr('data-sidemenu-theme');
-        const sideMenu = $('.fixed-menu');
-
-        if (menuTheme == 'shadowed') {
-            sideMenu.addClass('fixed-menu--shadowed');
-        } else {
-            sideMenu.removeClass('fixed-menu--shadowed');
-        }
-
-        display.css({
-            transform: `translateY(${position}%)`
-        });
-    
-        sections.eq(sectionEq).addClass('active').siblings().removeClass('active');
-
-        setTimeout(() => {
-            inScroll = false;
-
-            sideMenu
-            .find('.fixed-menu__item')
-            .eq(sectionEq)
-            .addClass('fixed-menu__item--active')
-            .siblings()
-            .removeClass('fixed-menu__item--active');
-            
-        }, 1300);
+    if (isNaN(position)) {
+        console.error('Передано неверное значение в countSectionPosition');
+        return 0;
     }
+
+    return position;
+};
+
+const changeMenuThemeForSection = sectionEq => {
+    const currentSection = sections.eq(sectionEq);
+    const menuTheme = currentSection.attr('data-sidemenu-theme');
+    const activeClass = 'fixed-menu--shadowed';
+
+    if (menuTheme == 'shadowed') {
+        sideMenu.addClass(activeClass);
+    } else {
+        sideMenu.removeClass(activeClass);
+    }
+};
+
+const resetActiveClassForItem = (items, itemEq, activeClass) => {
+    items.eq(itemEq).addClass(activeClass).siblings().removeClass(activeClass);
 }
 
-const scrollViewport = direction => {
+const performTransition = sectionEq => {
+    if (inScroll) return;
+
+    const transitionOver = 1000;
+    const mouseInertionOVer = 300;
+
+    inScroll = true;
+    const position = countSectionPosition(sectionEq);
+
+    changeMenuThemeForSection(sectionEq);
+
+    display.css({
+        transform: `translateY(${position}%)`
+    });
+
+    resetActiveClassForItem(sections, sectionEq, 'active');
+
+    setTimeout(() => {
+        inScroll = false;
+
+        resetActiveClassForItem(menuItems, sectionEq, 'fixed-menu__item--active');
+        
+    }, transitionOver + mouseInertionOVer);
+};
+
+const viewportScroller = () => {
     const activeSection = sections.filter('.active');
     const nextSection = activeSection.next();
     const prevSection = activeSection.prev();
 
-    if (direction == 'next' && nextSection.length) {
-        performTransition(nextSection.index());
-    }
-
-    if (direction == 'prev' && prevSection.length) {
-        performTransition(prevSection.index());
+    return {
+        next() {
+            if (nextSection.length) {
+                performTransition(nextSection.index());
+            }
+        },
+        prev() {
+            if (prevSection.length) {
+                performTransition(prevSection.index());
+            }
+        },
     }
 }
 
 $(window).on('wheel', e => {
     const deltaY =  e.originalEvent.deltaY;
+    const scroller = viewportScroller();
 
     if (deltaY > 0) {
-        scrollViewport('next');
+        scroller.next();
     }
 
     if (deltaY < 0) {
-        scrollViewport('prev');
+        scroller.prev();
     }
 });
 
 $(window).on('keydown', e => {
-
     const tagName = e.target.tagName.toLowerCase();
+    const userTypingInInputs = tagName == 'input' || tagName == 'textarea';
+    const scroller = viewportScroller();
 
-    if (tagName != 'input' && tagName != 'textarea') {
-        switch (e.keyCode) {
-            case 38: // prev
-                scrollViewport('prev');
-                break;
-            case 40:
-                scrollViewport('next');
-                break;
-        }
+    if (userTypingInInputs) return;
+
+    switch (e.keyCode) {
+        case 38: // prev
+            scroller.next();
+            break;
+        case 40:
+            scroller.prev();
+            break;
     }
 });
 
+$('.wrapper').on('touchmove', e => e.preventDefault());
+
 $('[data-scroll-to]').click(e => {
     e.preventDefault();
+
+    console.log("Clicked");
 
     const $this = $(e.currentTarget);
     const target = $this.attr('data-scroll-to');
@@ -92,3 +125,17 @@ $('[data-scroll-to]').click(e => {
     performTransition(reqSection.index());
 
 });
+
+if(isMobile) {
+    $('body').swipe({
+        swipe: function (event, direction) {
+            const scroller = viewportScroller();
+            let scrollDirection = '';
+    
+            if (direction == 'up') scrollDirection = 'next';
+            if (direction == 'down') scrollDirection = 'prev';
+    
+            scroller[scrollDirection]();
+        },
+    });
+}
--- scripts/player-html.js
+++ scripts/player-html.js
@@ -33,10 +33,40 @@ let eventsInit = () => {
         video[0].currentTime = newPlaybackPositionSec; 
     });
 
+    $('.volume-level').click(e => {
+        const volume = $(e.currentTarget);
+        const volumeClicked = e.originalEvent.layerX;
+        const newVolumePercent = (volumeClicked / volume.width()) * 100;
+        const newVolumeLevel = Math.round(volumeClicked / volume.width() * 10) / 10;
+
+        $('.volume-level__button').css({
+            left: `${newVolumePercent}%`,
+            marginLeft: '-7px'
+        });
+
+        $('.volume-level__actual').css({
+            width: `${newVolumePercent}%`
+        });
+
+        if (newVolumeLevel <= 0.09 ) {
+            video[0].volume = 0;
+            $('.player__volume').addClass('no-volume');
+        } else {
+            video[0].volume = newVolumeLevel;
+            $('.player__volume').removeClass('no-volume');
+        }
+    });
+
     $('.player__splash').click(e => {
         video[0].play();
         playerContainer.addClass('active');
         playerContainer.addClass('paused');
+    });
+
+    $('#html-player').click(e => {
+        video[0].pause();
+        playerContainer.removeClass('active');
+        playerContainer.removeClass('paused');
     });
 }
 
@@ -53,13 +83,34 @@ const formatTime = timeSec => {
     return `${minutes}:${seconds}`;
 }
 
-$( "document" ).ready(function() {
-    const playerSize = $('.player-section__container');
-    video[0].width = playerSize.width();
-    video[0].height = playerSize.height();
-});
-
 video.ready(function() {
+
+    const setInitialVolume = () => {
+        video[0].volume = 0.4;
+
+        $('.volume-level__button').css({
+            left: "40%",
+            marginLeft: '-7px'
+        });
+        $('.volume-level__actual').css({
+            width: "40%"
+        });
+    }
+
+    setInitialVolume();
+
+    const formatTime = timeSec => {
+    const roundTime = Math.round(timeSec);
+
+    const minutes = addZero(Math.floor(roundTime / 60));
+    const seconds = addZero(roundTime % 60);
+
+    function addZero(num) {
+        return num < 10 ? `0${num}` : num;
+    }
+
+    return `${minutes}:${seconds}`;
+}
     let interval;
     const durationSec = video[0].duration;
 
@@ -88,6 +139,18 @@ video.ready(function() {
         }
 
     }, 1000); 
+});
+
+$('document').ready(function() {
+    const playerSize = $('.player-section__container');
+    video[0].width = playerSize.width();
+    video[0].height = playerSize.height();
+});
+
+$( window ).resize(function() {
+    const playerSize = $('.player-section__container');
+    video[0].width = playerSize.width();
+    video[0].height = playerSize.height();
 });
 
 eventsInit();
